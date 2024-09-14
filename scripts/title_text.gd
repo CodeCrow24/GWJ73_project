d98729dcade316d738787a82a42cb7e38d1190e3
Leo <leo.rahden@gmail.com>
2024-09-14 17:30:29 +0200

    reworked title screen, ready for levels

--- scripts/title_text.gd
+++ scripts/title_text.gd
@@ -2,59 +2,41 @@ extends Node2D
 var mouseOnPlay = false
 var mouseOnOpt = false
 var mouseOnCred = false
+var mouseOnBack = false
 var showTitle = true
-var PlaySpeed = 0
-var OptSpeed = 0
-var CredSpeed = 0
 var titleVis = true
-var index = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Title.position.x = 0
-	$Play_Button.position.x = 0
-	$Options_Button.position.x = 0
-	$Credits_Button.position.x = 0
+	pass
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	checkTitleVis()
-	moveTitle(delta)
-
-
-func moveTitle(deltaTime):
-	PlaySpeed = 0
-	OptSpeed = 0
-	CredSpeed = 0
-	
-	
-	if index <= 3 and showTitle != titleVis: 
-		index += 0.05
-		
-	if !showTitle and titleVis:
-		if index >= 1:
-			PlaySpeed = -600
-		if index >= 2:
-			OptSpeed = -600
-		if index >= 3:
-			CredSpeed = -600
-	
-	$Play_Button.position.x += PlaySpeed * deltaTime
-	$Options_Button.position.x += OptSpeed * deltaTime
-	$Credits_Button.position.x += CredSpeed * deltaTime
+	updateTitleVis()
 			
+func updateTitleVis():
+	if showTitle != titleVis:
+		if titleVis:
+			$Play_Button.hide()
+			$Options_Button.hide()
+			$Credits_Button.hide()
+			$back_button.show()
+		else: 
+			$Play_Button.show()
+			$Options_Button.show()
+			$Credits_Button.show()
+			$back_button.hide()
 
 
 func checkTitleVis():
-	if $Play_Button.position.x <= -324 and $Options_Button.position.x <= -324 and $Credits_Button.position.x <= -324:
-		$Play_Button.position.x = -324
-		$Options_Button.position.x = -324
-		$Credits_Button.position.x = -324
+	if $Play_Button.is_visible() and $Options_Button.is_visible() and $Credits_Button.is_visible():
+		titleVis = true
+	else:
 		titleVis = false
-		index = 0
 
 
 func _on_play_mouse_entered():
@@ -81,6 +63,16 @@ func _on_cred_mouse_exited():
 	mouseOnCred = false
 
 
+func _on_back_mouse_entered():
+	mouseOnBack = true
+
+
+func _on_back_mouse_exited():
+	mouseOnBack = false
+
+
 func _input(_event):
 	if Input.is_action_just_pressed("mouse_left") and (mouseOnPlay or mouseOnCred or mouseOnOpt):
 		showTitle = false
+	if Input.is_action_just_pressed("mouse_left") and (mouseOnBack):
+		showTitle = true
