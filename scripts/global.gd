f32185ddd3dc3690b41f63c5244ce7a56eb018f0
Leo <leo.rahden@gmail.com>
2024-09-14 10:46:16 +0200

    created global script and input map

---
+++ scripts/global.gd
@@ -0,0 +1,17 @@
+extends Node
+#	Put global variables in here, you can access this from any scene or node
+#	in other scripts type global.variableName to access your variable
+
+
+
+
+
+
+# Called when the global node is loaded
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass # Replace with function body.
