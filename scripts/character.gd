0138fa2b28629e499557c4ccccd617ad4a58012c
Leo <leo.rahden@gmail.com>
2024-09-15 20:28:34 +0200

    implemented custom character

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-const SPEED = 100
+@export var SPEED = 50
 var player_state
+var last_dir = "s-idle"
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -18,22 +19,22 @@ func _physics_process(delta):
 	
 func play_animation(dir):
 	if player_state== "idle":
-		$anim.play("idle")
+		$anim.play(last_dir)
 	elif player_state == "walk":
 		if dir.y == -1 and dir.x == 0:
 			$anim.play("n-walk")
+			last_dir = "n-idle"
 		elif dir.y == 1 and dir.x == 0:
 			$anim.play("s-walk")
+			last_dir = "s-idle"
 		elif dir.x == -1 and dir.y == 0:
 			$anim.play("w-walk")
+			last_dir = "w-idle"
 		elif dir.x == 1 and dir.y == 0:
 			$anim.play("e-walk")
-		elif dir.y < -0.5 and dir.x > 0.5:
-			$anim.play("ne-walk")
-		elif dir.y > 0.5 and dir. x > 0.5:
-			$anim.play("se-walk")
-		elif dir.y > 0.5 and dir.x < -0.5:
-			$anim.play("sw-walk")
-		elif dir.y < -0.5 and dir.x < -0.5:
-			$anim.play("nw-walk")
+			last_dir = "e-idle"
 		
+
+func _input(event):
+	if Input.is_action_just_pressed("mouse_right"):
+		position = get_global_mouse_position()
