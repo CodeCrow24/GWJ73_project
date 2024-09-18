dca95612397d0dfb5aa79928d97f9b86ede7ed33
PolyGoneAway <polygoneawaydev@gmail.com>
2024-09-18 21:01:33 +0200

    Funfact system update

--- scripts/fun_fact_panel.gd
+++ scripts/fun_fact_panel.gd
@@ -4,16 +4,28 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	
+<<<<<<< Updated upstream
 	pass
 
 func show_funfact(funfact):
 	visible = true
 	
 	$FunFactLabel.text = funfact
+=======
+	show_funfact(Global.FunFacts[0])
+
+func show_funfact(funfact):
+	visible = true
+	if Global.FunFacts.size() > 0:
+		$FunFactLabel.text = funfact
+>>>>>>> Stashed changes
 
 func hide_funfact():
 	visible = false
 	
+<<<<<<< Updated upstream
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("exit_menu"):
 		hide_funfact()
+=======
+>>>>>>> Stashed changes
--- scripts/global.gd
+++ scripts/global.gd
@@ -5,10 +5,15 @@ var Transition_Shader_Progress = 1 # Transition Black Fade
 var showTransitionFade = false
 var TransitionShown = false
 
+<<<<<<< Updated upstream
 # QUEST SYSTEM VARIABLES #
 var quests = [["getCrate", "goToNpc"], ["getCrate", "getCrate", "goToNpc"]]
 var currentPlayerQuestStep
 var closestNpcQuestIndex
+=======
+@export var FunFacts: Array[String] = ["...that the first recorded recipe is over 4000 Years old", "...that the longest english word without a vowel is rhythms"] # Did you know ... //
+
+>>>>>>> Stashed changes
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept"):
