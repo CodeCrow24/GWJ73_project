e9f2bc0e7f2dd62630bf19f64c7eba82768a52df
PolyGoneAway <polygoneawaydev@gmail.com>
2024-09-22 18:04:06 +0200

    lol i had no merging issues

--- scripts/30fps_big_bababubu_keyboard.gd
+++ scripts/30fps_big_bababubu_keyboard.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+#30 fps (ik now its 15 but no will notice)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("rgb")
